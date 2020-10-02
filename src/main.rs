731e5e1a50bc33cf3a833d033a4e0abd5a3d6c67
Aleksei Kokovin <kokovin@proitr.ru>
2020-10-02 21:37:05 +0500

    get args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::env;
+
+fn about() {
     println!("Rust Snitch by Kokovin Aleksei");
+    println!("I will hopefully write about the args here");
+    println!("I'm sorry if I didn't");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("I expected some args!");
+        about();
+    }
 }
