565c2389a47021d5f017c4afa605566f141d4a1f
Aleksei Kokovin <kokovin@proitr.ru>
2020-10-10 22:29:12 +0500

    use aho-corasick to find patterns

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,20 @@ use std::process::exit;
 use std::fs::File;
 use std::path::Path;
 use std::io::{self, BufRead};
+use aho_corasick::AhoCorasick;
+use std::borrow::Borrow;
+
+#[derive(Debug)]
+pub struct CommandMatch {
+    line: String,
+    line_idx: i32,
+    file: String
+}
+
+pub struct InputFile {
+    file: String,
+    lines: io::Lines<io::BufReader<File>>
+}
 
 pub fn about() -> Result<String, String> {
     let message =   "Rust Snitch by Kokovin Aleksei \n\
@@ -14,23 +28,19 @@ pub fn about() -> Result<String, String> {
     return Ok(message.to_string());
 }
 
-fn read_file(paths: Vec<String>) -> Result<String, String> {
-    let mut message = String::new();
+fn read_file<'a>(paths: Vec<String>) -> Result<Vec<InputFile>, String> {
+    let mut files = vec![];
     for path in paths {
-        let res = read_lines(path);
+        let res = read_lines(path.clone());
         match res {
             Ok(lines) => {
-                for line in lines {
-                    if let Ok(ip) = line {
-                        message = message + &*ip + "\n";
-                    }
-                }
+                files.push(InputFile{ file: path, lines })
             },
             Err(er) =>
                 return Err(format!("{:?}", er))
         }
     };
-    return  Ok(message);
+    return  Ok(files);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -40,7 +50,31 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
         Ok(file) => Ok(io::BufReader::new(file).lines()),
         Err(e) => Err(e)
     }
+}
 
+fn search_patterns(patterns: Vec<String>,
+                       lines: io::Lines<io::BufReader<File>>,
+                       file_name: String) -> Result<Vec<CommandMatch>, String> {
+    let ac = AhoCorasick::new(patterns);
+    let mut matches = vec![];
+    for line_res in lines {
+        match line_res {
+            Ok(line) => {
+                let res = ac.find(line.clone());
+                if res.is_some() {
+                    matches.push(CommandMatch {
+                        line: line.clone(),
+                        line_idx: 0,
+                        file: file_name.clone()
+                    })
+                }
+            }
+            Err(er) => {
+                return Err(format!("{:?}", er))
+            }
+        }
+    }
+    return Ok(matches);
 }
 
 fn main() {
@@ -56,18 +90,18 @@ fn main() {
             .multiple(true)
             .takes_value(true)).get_matches();
     if matches.value_of("files").is_some() {
-        let result = read_file(
+        let files = read_file(
             matches.value_of("files").unwrap()
                 .split(",").map(|x| x.to_string()).collect());
-        match result {
-            Ok(message) => {
-                println!("{}", message)
-            },
-            Err(error_message) => {
-                eprintln!("{}", error_message);
-                exit(1)
+        let patterns = vec!["TODO".to_string()];
+        let mut result = vec![];
+        for file in files.unwrap() {
+            let file_pattern_matched = search_patterns(patterns.clone(), file.lines, file.file);
+            if file_pattern_matched.is_ok() {
+                result.append(&mut file_pattern_matched.unwrap());
             }
         }
+        println!("{:?}", result);
     } else {
 
     }
