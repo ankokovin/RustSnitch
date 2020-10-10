64bde0f6dedf95692d7cf5c2f0cef176d8e7d56d
Aleksei Kokovin <kokovin@proitr.ru>
2020-10-10 19:38:25 +0500

    #5 refactor to use Result type and not custom struct

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,14 @@ use std::fs::File;
 use std::path::Path;
 use std::io::{self, BufRead};
 
-
-#[derive(Debug, Clone, PartialOrd, PartialEq)]
-pub enum ResultType {
-    OK,Fail
-}
-
-#[derive(Debug, Clone)]
-pub struct ProgramResult {
-    type_result: ResultType,
-    message: Option<String>,
-}
-
-pub fn about() -> Box<ProgramResult> {
+pub fn about() -> Result<String, String> {
     let message =   "Rust Snitch by Kokovin Aleksei \n\
                         I will hopefully write about the args here\n\
                         I'm sorry if I didn't".to_string();
-    let type_result = ResultType::OK;
-    return Box::new(ProgramResult { type_result, message: Some(message) });
+    return Ok(message.to_string());
 }
 
-fn read_file(paths: Vec<String>) -> Box<ProgramResult> {
+fn read_file(paths: Vec<String>) -> Result<String, String> {
     let mut message = String::new();
     for path in paths {
         let res = read_lines(path);
@@ -37,13 +24,12 @@ fn read_file(paths: Vec<String>) -> Box<ProgramResult> {
                         message = message + &*ip + "\n";
                     }
                 }
-            }
-            Err(er) => {
-                return Box::new(ProgramResult { type_result: ResultType::Fail, message: Option::from(format!("{:?}", er)) });
-            }
+            },
+            Err(er) =>
+                return Err(format!("{:?}", er))
         }
-    }
-    return  Box::new(ProgramResult { type_result: ResultType::OK, message: Option::from(message) });
+    };
+    return  Ok(message);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -56,32 +42,29 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 
 }
 
-fn act_result(result: Box<ProgramResult>) {
-    let default_message = "Unknown error".to_string();
-    println!("{}", result.message.unwrap_or(default_message));
-    match result.type_result {
-        ResultType::OK => exit(0),
-        ResultType::Fail => exit(1),
-    }
-}
-
-fn run(args: Vec<String>) -> Box<ProgramResult> {
-    let result: Box<ProgramResult> = Box::new(ProgramResult { type_result: ResultType::Fail, message: None });
+fn run(args: Vec<String>) -> Result<String, String> {
     if args.len() == 1 {
         println!("I expected some args!");
         return about();
     }
     if args[1] == "-f" {
         if args.len() == 2 {
-            return Box::new(ProgramResult{type_result: ResultType::Fail, message: Option::from("Expected path to file".to_string()) })
+            return Err("Expected path to file".to_string())
         }
         return read_file(args[2..].to_owned());
     }
-    return result;
+    return Err("Could not parse args!".to_string());
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let result = run(args);
-    act_result(result);
+    match run(args) {
+        Ok(message) => {
+            println!("{}", message);
+        },
+        Err(error_message) => {
+            println!("{}", error_message);
+            exit(1);
+        }
+    }
 }
